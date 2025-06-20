da702276ea5b96da40e36537b3a2a9dd93d9b1da
Farhan Yousafzai <coderwithferry@gmail.com>
2025-06-21 01:09:50 +0500

    Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from './Pages/Home'
+import './App.css'
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import gsap from 'gsap'
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
+
 
 const App = () => {
   return (
     <>
-      <Home/>
+   <div className='w-screen h-screen bg-amber-500 flex-center text-8xl '>Hello,Gsap</div>
     </>
   )
 }
